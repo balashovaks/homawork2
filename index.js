0f9caba590a184110239dd67a3d30ec2256de7f2
Kseniya Balashova <balashova.ksenya@bk.ru>
2021-11-05 19:20:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"кот" > "код" //true
+"2" + 2 * "2" //24
+undefined == null //true
+undefined === null //false
+null == 0 //false
+2 > "3" //false
+null - false + true //1
+1 / "l" //NaN
+"2" * "3" //6
+4 + 5 + "O" //90
+"l" + 4 + 5 //145
+"4" - 2 //2
+"4" - "4x" //NaN
+null == false //false
+" -4 "/ 0 + 1 //-Infinity
+null + 1 //1
+undefined + null //NaN
